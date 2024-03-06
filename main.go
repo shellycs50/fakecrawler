f06e4dbd1cd8cd0d5b20a9e897d5004f3a6eeaf2
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-03-06 17:58:04 +0000

    implemented concurrent url fetching with colly

--- main.go
+++ main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
+
+	"github.com/gocolly/colly"
 )
 
-type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
-	Fetch(url string) (body string, urls []string, err error)
-}
+// type Fetcher interface {
+// 	// Fetch returns the body of URL and
+// 	// a slice of URLs found on that page.
+// 	Fetch(url string) (body string, urls []string, err error)
+// }
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher, wg *sync.WaitGroup, prevFetchedUrls *map[string]struct{}, mu *sync.Mutex) {
+func Crawl(url string, depth int, fetcher func(string) (string, []string, error), wg *sync.WaitGroup, prevFetchedUrls *map[string]struct{}, mu *sync.Mutex, data *[]byte) {
 	// TODO: Fetch URLs in parallel.
 	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
 	defer wg.Done()
+
+	fmt.Println("Fetching urls from ", url)
 
 	if depth <= 0 {
 		return
@@ -25,82 +30,50 @@ func Crawl(url string, depth int, fetcher Fetcher, wg *sync.WaitGroup, prevFetch
 
 	mu.Lock()
 	if _, ok := (*prevFetchedUrls)[url]; ok {
+		// fmt.Print("*******\n\n\n\n\n*******Already Visited*******\n\n\n\n\n*******\n")
 		mu.Unlock()
 		return
 	}
+	*data = append(*data, []byte(url+"\n")...)
 	(*prevFetchedUrls)[url] = struct{}{}
+
 	mu.Unlock()
 
-	body, urls, err := fetcher.Fetch(url)
+	_, urls, err := fetcher(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
 	for _, u := range urls {
 		wg.Add(1)
-		go Crawl(u, depth-1, fetcher, wg, prevFetchedUrls, mu)
+		go Crawl(u, depth-1, fetcher, wg, prevFetchedUrls, mu, data)
 	}
-	// struct{} is the data type {} is the value
-	(*prevFetchedUrls)[url] = struct{}{}
+
 }
 
 func main() {
 	var urls = make(map[string]struct{})
+	var data = []byte("")
 	// using mutex to lock the map, using map for constant time lookups. (previously used slice but with the amount of blocking you could argue quicker with a channel for small datasets but for large datasets a map is quicker.)
 	var mu sync.Mutex
 	var wg sync.WaitGroup
 	wg.Add(1)
-	Crawl("https://golang.org/", 5, fetcher, &wg, &urls, &mu)
+	Crawl("https://io-academy.uk/", 5, Fetch, &wg, &urls, &mu, &data)
 	wg.Wait()
-	fmt.Printf("%v", urls)
+	os.WriteFile("./urls.txt", data, 0644)
 }
 
-// fakeFetcher is Fetcher that returns canned results.
-type fakeFetcher map[string]*fakeResult
-
-type fakeResult struct {
-	body string
-	urls []string
-}
-
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-	if res, ok := f[url]; ok {
-		return res.body, res.urls, nil
-	}
-	return "", nil, fmt.Errorf("not found: %s", url)
-}
-
-// fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-	"https://golang.org/": &fakeResult{
-		"The Go Programming Language",
-		[]string{
-			"https://golang.org/pkg/",
-			"https://golang.org/cmd/",
-		},
-	},
-	"https://golang.org/pkg/": &fakeResult{
-		"Packages",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/cmd/",
-			"https://golang.org/pkg/fmt/",
-			"https://golang.org/pkg/os/",
-		},
-	},
-	"https://golang.org/pkg/fmt/": &fakeResult{
-		"Package fmt",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
-		},
-	},
-	"https://golang.org/pkg/os/": &fakeResult{
-		"Package os",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
-		},
-	},
+func Fetch(url string) (body string, urls []string, err error) {
+	var url_list []string
+	c := colly.NewCollector()
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if len(link) > 5 && link[:5] == "https" {
+			url_list = append(url_list, link)
+		}
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+	})
+	c.Visit(url)
+	return "", url_list, nil
 }
