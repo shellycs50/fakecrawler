7fa830b69af69b92d5989480ba64bc3b5453fb9a
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-03-07 22:25:17 +0000

    fixed implementation of checking if previously searched

--- main.go
+++ main.go
@@ -23,20 +23,27 @@ func Crawl(url string, depth int, fetcher Fetcher, wg *sync.WaitGroup, prevFetch
 
 	defer wg.Done()
 
-	fmt.Println("Fetching urls from ", url)
-
 	if depth <= 0 {
 		return
 	}
 
-	_, urls, err := fetcher.Fetch(url, mu, data, prevFetchedUrls)
+	_, foundUrls, err := fetcher.Fetch(url, mu, data, prevFetchedUrls)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, u := range urls {
-		wg.Add(1)
-		go Crawl(u, depth-1, fetcher, wg, prevFetchedUrls, mu, data)
+	for _, newUrl := range foundUrls {
+		shouldCrawl := false
+		mu.Lock()
+		if _, ok := (*prevFetchedUrls)[newUrl]; !ok {
+			(*prevFetchedUrls)[newUrl] = struct{}{}
+			shouldCrawl = true
+		}
+		mu.Unlock()
+		if shouldCrawl {
+			wg.Add(1)
+			go Crawl(newUrl, depth-1, fetcher, wg, prevFetchedUrls, mu, data)
+		}
 	}
 
 }
@@ -49,7 +56,7 @@ func main() {
 	var wg sync.WaitGroup
 	var this_is_confusing Fetcher = myFirstFetchStruct{}
 	wg.Add(1)
-	Crawl("https://io-academy.uk/", 9, this_is_confusing, &wg, &urls, &mu, &data)
+	Crawl("https://muz.li/blog/60-most-creative-portfolio-websites-of-2023", 5, this_is_confusing, &wg, &urls, &mu, &data)
 	wg.Wait()
 	os.WriteFile("./urls.txt", data, 0644)
 }
@@ -57,14 +64,13 @@ func main() {
 func (f myFirstFetchStruct) Fetch(url string, mu *sync.Mutex, data *[]byte, prevFetchedUrls *map[string]struct{}) (body string, urls []string, err error) {
 	var url_list []string
 	c := colly.NewCollector()
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	c.OnResponse(func(*colly.Response) {
+		fmt.Printf("Visited %v\n", url)
 		mu.Lock()
-		if _, ok := (*prevFetchedUrls)[url]; !ok {
-			*data = append(*data, []byte(url+"\n")...)
-			(*prevFetchedUrls)[url] = struct{}{}
-		}
+		*data = append(*data, []byte(url+"\n")...)
 		mu.Unlock()
+	})
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		url_list = append(url_list, link)
 	})
